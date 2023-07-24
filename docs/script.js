440401b36bf686e13b80f726ee173ac53b4c3d37
PeterM <hello.peterm@gmail.com>
2023-07-24 07:14:16 -0700

    Added sample code

---
+++ docs/script.js
@@ -0,0 +1,8 @@
+// This is how to comment in JavaScript
+
+// Link to external JQuery
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js">
+
+$('div').on('click', function() {
+    $(this).toggleClass('show-description');
+});
