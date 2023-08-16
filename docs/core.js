eac39dedf5263862a76b895b163a062e16703b9e
hello-peterm <hello.peterm@gmail.com>
2023-08-16 13:33:56 -0700

    Refactoring, splitting out functions

--- docs/core.js
+++ docs/core.js
@@ -1,19 +1,36 @@
+// generate a random in integer for the computer's guess
 const computerChooses = () => {
     randomNum = Math.floor(Math.random() * 3);
     return randomNum
   }
 
-  module.exports = computerChooses;
-
-  /*const playersAnswer = (answer) => {
-    const previousAnswer = document.querySelector("h3");
-    if (previousAnswer) {
-      previousAnswer.remove()
-    }
-    player.playerChoice = options.indexOf(answer);
-    computer.computerChoice = computerChooses();
-    compareChoices(computer.computerChoice,player.playerChoice);
+// determine the winner by comparing the hierarchy of the choices
+const compareChoices = (computer, user) => {
+  message = "The computer chose " + options[computer.computerChoice] + " and the player chose " + options[player.playerChoice] + ".";
+  if ((computer == 1 && user == 3) || (computer == 2 && user == 1) || (computer == 3 && user == 2)) {
+    return "computer";
   }
+  else if (computer == user) {
+    return 0
+  }
+  else {
+    return "player";
+  }
+}
 
-  module.exports = playersAnswer;*/
+// generate a string declaring a winner and the selections the player's made
+const resultMessage = (winner) => {
+  message = "The computer chose " + options[computer.computerChoice] + " and the player chose " + options[player.playerChoice] + ".";
+  if (winner === 0) {
+    message = "It is a tie!";
+    displayResult(message);
+  }
+  else {
+    displayResult("The " + winner + " wins! " + message);
+  }
+};
+
+  module.exports = computerChooses;
+  module.exports = compareChoices;
+  module.exports = resultMessage;
 
--- docs/script.js
+++ docs/script.js
@@ -8,60 +8,37 @@ const computer = {
   computerChoice : null
 }
 
-let playerGuess = 0;
-
-player.playerChoice = playerGuess;
-
-const result = (message) => {
-  const announcement = document.createElement('h3');
-  announcement.innerText = message
-  const page = document.querySelector('#resultAnswer');
-  page.append(announcement);
+const playerChoseRock = () => {
+  submitPlayersAnswer("Rock");
 }
 
-const compareChoices = (comp, user) => {
-  message = "The computer chose " + options[computer.computerChoice] + " and the player chose " + options[player.playerChoice] + ".";
-  if ((comp == 1 && user == 3) || (comp == 2 && user == 1) || (comp == 3 && user == 2)) {
-    const winner = "computer";
-    result ("The " + winner + " wins! " + message);
-}
-  else if (comp == user) {
-    message = "It is a tie!";
-    result(message);
-}
-  else {
-    winner = "player";
-    result ("The " + winner + " wins! " + message);
-}
-}
-;
-
-const choseRock = () => {
-  const answer = "Rock";
-  playersAnswer(answer);
+const playerChosePaper = () => {
+  submitPlayersAnswer("Paper");
 }
 
-const chosePaper = () => {
-  const answer = "Paper";
-  playersAnswer(answer);
+const playerChoseScissors = () => {
+  submitPlayersAnswer("Scissors");
 }
 
-const choseScissors = () => {
-  const answer = "Scissors"
-  playersAnswer(answer);
-}
+document.querySelector("#rock").addEventListener("click", playerChoseRock);
+document.querySelector("#paper").addEventListener("click", playerChosePaper);
+document.querySelector("#scissors").addEventListener("click", playerChoseScissors);
 
-document.querySelector("#rock").addEventListener("click", choseRock);
-document.querySelector("#paper").addEventListener("click", chosePaper);
-document.querySelector("#scissors").addEventListener("click", choseScissors);
-
-
-const playersAnswer = (answer) => {
+// clear the previous message and submit the answers for comparision
+const submitPlayersAnswer = (answer) => {
   const previousAnswer = document.querySelector("h3");
   if (previousAnswer) {
     previousAnswer.remove()
   }
   player.playerChoice = options.indexOf(answer);
   computer.computerChoice = computerChooses();
-  compareChoices(computer.computerChoice,player.playerChoice);
+  resultMessage(compareChoices(computer.computerChoice, player.playerChoice));
+}
+
+// display the winner and description in a new header element
+const displayResult = (message) => {
+  const announcement = document.createElement('h3');
+  announcement.innerText = message
+  const page = document.querySelector('#resultAnswer');
+  page.append(announcement);
 }
